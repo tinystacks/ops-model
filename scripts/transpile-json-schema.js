ab22c193a62574af011bffa9b0f222b7882bd0a7
Caleb Courier <caleb.courier@gmail.com>
2023-02-06 14:27:30 -0600

    cleanup

--- scripts/transpile-json-schema.js
+++ scripts/transpile-json-schema.js
@@ -4,8 +4,6 @@ const yaml = require('js-yaml');
 const { resolveRefsAt } = require('json-refs');
 
 async function transpileJsonSchema () {
-  const yamlSchema = fs.readFileSync('src/schemas/config.yml');
-
   const refResolvedSchema = await resolveRefsAt(path.resolve('src/schemas/config.yml'),  {
     loaderOptions : {
       processContent: (res, callback) => {
@@ -13,8 +11,6 @@ async function transpileJsonSchema () {
       }
     }
   });
-
-  // console.debug('')
 
   fs.writeFileSync('dist/config-schema.json', JSON.stringify(refResolvedSchema.resolved.Config, null, 2));
 }
